d406c58e4e6400edb02ce3d7267a2cabcdc86aa1
treeDweller98 <personal.fahim00@gmail.com>
2021-12-09 20:11:13 +0600

    usb actually works now

--- app/src/main/java/com/example/maskapp/MainActivity.java
+++ app/src/main/java/com/example/maskapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
@@ -58,12 +59,12 @@ public class MainActivity extends AppCompatActivity {
     private final byte NOT_MASKED = 78;                  // 'N' SEND TO ARDUINO TO DENY ENTRY
 
     public final int BAUD_RATE = 9600;
-    public final String ACTION_USB_PERMISSION = "USB_PERMISSION";
+    public final String ACTION_USB_PERMISSION = "com.example.maskapp.USB_PERMISSION";
 
     private final int CAMERA_PERM_CODE = 1;
-    private final float REAR_CAM_ROTATE_ANGLE = 90;           // on my redmi note 6 pro
+    private final float REAR_CAM_ROTATE_ANGLE = 90;             // on my redmi note 6 pro
     private final float FRONT_CAM_ROTATE_ANGLE = -90;           // on my redmi note 6 pro
-
+    private final int ARDUINO_VID = 0x1A86;                           // my CH340 nano v3
 
     SurfaceTexture surfaceTexture;
     Camera camera;
@@ -77,79 +78,10 @@ public class MainActivity extends AppCompatActivity {
     UsbSerialDevice serialPort;
     UsbDeviceConnection connection;
 
-    Button detectButton, connectButton;
+    Button detectButton, startButton, stopButton;
     ToggleButton cameraSelector;
     ImageView imageView;
     TextView statusText;
-
-    // ARDUINO CONNECTION
-    /** Receives data from Arduino and triggers action */
-    UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() { //Defining a Callback which triggers whenever data is read.
-        @Override
-        public void onReceivedData(byte[] arg0) {
-            byte data = arg0[0] ;
-            if ( data == PERSON_DETECTED ) {
-                detectButton.performClick();        // i.e. when IR detects person
-            } else {
-                Toast.makeText( getBaseContext(), "Corrupt data returned by Arduino" + data, Toast.LENGTH_LONG ).show();
-            }
-            /*
-            String data = null;
-            try {
-                data = new String( arg0, "UTF-8" );
-                if ( data[0].equals( PERSON_DETECTED ) ) {
-                    detectButton.performClick();        // i.e. when IR detects person
-                }
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-                Toast.makeText( getBaseContext(), "Corrupt data returned by Arduino" , Toast.LENGTH_LONG ).show();
-            }*/
-        }
-    };
-
-    /** Asks permission, opens and closes connection (if device is arduino) */
-    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
-                boolean granted = intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
-                if (granted) {
-                    connection = usbManager.openDevice(device);
-                    serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
-                    if (serialPort != null) {
-                        if (serialPort.open()) { //Set Serial Connection Parameters.
-                            serialPort.setBaudRate( BAUD_RATE );
-                            serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
-                            serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
-                            serialPort.setParity(UsbSerialInterface.PARITY_NONE);
-                            serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
-                            serialPort.read(mCallback);
-
-                            statusText.setText( "Serial Connection Open." );
-                            Toast.makeText( getBaseContext(), "Arduino Connected: system is active." , Toast.LENGTH_LONG ).show();
-                        } else {
-                            Log.d("SERIAL", "PORT NOT OPEN");
-                            Toast.makeText( getBaseContext(), "SERIAL: Port not open" , Toast.LENGTH_LONG ).show();
-                        }
-                    } else {
-                        Log.d("SERIAL", "PORT IS NULL");
-                        Toast.makeText( getBaseContext(), "SERIAL: Port is NULL" , Toast.LENGTH_LONG ).show();
-                    }
-                } else {
-                    Log.d("SERIAL", "PERM NOT GRANTED");
-                    Toast.makeText( getBaseContext(), "SERIAL: USB permission not granted" , Toast.LENGTH_LONG ).show();
-                }
-            }
-            else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_ATTACHED)) {
-                connectButton.performClick();
-            }
-            else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED)) {
-                serialPort.close();
-                statusText.setText( "Serial Connection Closed." );
-                Toast.makeText( getBaseContext(), "Arduino connection closed: system is inactive." , Toast.LENGTH_LONG ).show();
-            }
-        }
-    };
 
 
     @Override
@@ -159,15 +91,13 @@ public class MainActivity extends AppCompatActivity {
 
         // Prepare the app
         {
-            // Prepare USB manager
-            usbManager= (UsbManager) getSystemService(this.USB_SERVICE);
-
             // Initialise app UI
-            detectButton = (Button) findViewById(R.id.detect);
-            connectButton = (Button) findViewById(R.id.arduinoConnectBtn);
+            detectButton = (Button) findViewById(R.id.detectButton );
+            startButton = (Button) findViewById(R.id.startButton );
+            stopButton = (Button) findViewById(R.id.stopButton);
             cameraSelector = (ToggleButton) findViewById(R.id.cameraToggle);
             imageView = (ImageView) findViewById(R.id.image);
-            statusText = (TextView) findViewById(R.id.textView); statusText.setText( "Connect Arduino via USB and press ACTIVATE" );
+            statusText = (TextView) findViewById(R.id.textView); statusText.setText( "Connect Arduino via USB and press CONNECT" );
 
             // Initialise camera
             if(ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
@@ -196,6 +126,15 @@ public class MainActivity extends AppCompatActivity {
             imageProcessor = new ImageProcessor.Builder()
                     .add(new ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
                     .build();
+
+            // Prepare USB manager
+            usbManager= (UsbManager) getSystemService(this.USB_SERVICE);
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(ACTION_USB_PERMISSION);
+            filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+            filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+            registerReceiver(broadcastReceiver, filter);
+            setUiEnabled(false);
         }
 
         // Camera selector (default is rear, rotate angle 90)
@@ -227,67 +166,127 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-        // Take photo and run inference on click
-        detectButton.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                camera.startPreview();
-                camera.takePicture( null, null, jpegCallback );
-            }
-        } );
-
-        // Connect/Disconnect to Arduino on click
-        connectButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
-                if (!usbDevices.isEmpty()) {
-                    boolean keep = true;
-                    for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
-                        device = entry.getValue();
-                        int deviceVID = device.getVendorId();
-                        if (deviceVID == 0x2341) { //Arduino Vendor ID
-                            PendingIntent pi = PendingIntent.getBroadcast( getBaseContext(), 0, new Intent(ACTION_USB_PERMISSION), 0);
-                            usbManager.requestPermission(device, pi);
-                            keep = false;
-                        } else {
-                            connection = null;
-                            device = null;
-                        }
-                        if (!keep) break;
-                    }
-                } else {
-                    Toast.makeText( getBaseContext(), "Please connect a device first" , Toast.LENGTH_SHORT ).show();
-                }
-            }
-        });
     }
 
 
-    // INFERENCE RELATED METHODS
-    /** Sets the image to the bitmap and ImageView, calls inference */
+    /** Receives data from Arduino and triggers action */
+    UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() {
+        //Defining a Callback which triggers whenever data is read.
+        @Override
+        public void onReceivedData( byte[] arg0 ) {
+            String data = null;
+            try {
+                data = new String( arg0, "UTF-8" );
+                tvAppend( statusText, data + " raw: " + arg0 + "\n" );
+                onClickDetect( detectButton );
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    /** Sets the image to the bitmap and ImageView, calls inference      NEEDS UPDATES */
     Camera.PictureCallback jpegCallback = new Camera.PictureCallback() {
         public void onPictureTaken(final byte[] data, Camera camera) {
             bitmap = BitmapFactory.decodeByteArray( data, 0, data.length  );
             bitmap = rotateImage( bitmap, rotateAngle );
             imageView.setImageBitmap( bitmap );
+            sendDoorSignal();
+        }
+    };
 
-            if ( doInference() ) {
-                Toast.makeText( getBaseContext(), "Positive detection" , Toast.LENGTH_LONG ).show();
-                if ( serialPort != null && serialPort.isOpen() ) {
-                    serialPort.write( new byte[]{YES_MASKED} );
-                    Toast.makeText( getBaseContext(), "SENT MASK YAY UwU" , Toast.LENGTH_LONG ).show();
+
+    // ARDUINO CONNECTION
+    /** Asks permission, opens and closes connection (if device is arduino) */
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        //Broadcast Receiver to automatically start and stop the Serial connection.
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
+                boolean granted =
+                        intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
+                if (granted) {
+                    connection = usbManager.openDevice(device);
+                    serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
+                    if (serialPort != null) {
+                        if (serialPort.open()) { //Set Serial Connection Parameters.
+                            setUiEnabled(true); //Enable Buttons in UI
+                            serialPort.setBaudRate( BAUD_RATE );
+                            serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                            serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
+                            serialPort.setParity(UsbSerialInterface.PARITY_NONE);
+                            serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+                            serialPort.read(mCallback); //
+                            tvAppend( statusText,"Serial Connection Opened!\n");
+
+                        } else {
+                            Log.d("SERIAL", "PORT NOT OPEN");
+                        }
+                    } else {
+                        Log.d("SERIAL", "PORT IS NULL");
+                    }
+                } else {
+                    Log.d("SERIAL", "PERM NOT GRANTED");
                 }
-            } else {
-                Toast.makeText( getBaseContext(), "Negative detection" , Toast.LENGTH_LONG ).show();
-                if ( serialPort != null && serialPort.isOpen() ) {
-                    serialPort.write( new byte[]{NOT_MASKED} );
-                    Toast.makeText( getBaseContext(), "SENT MASK NAY TwT" , Toast.LENGTH_LONG ).show();
-                }
+            }
+            else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_ATTACHED)) {
+                onClickStart(startButton);
+            }
+            else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED)) {
+                onClickStop(stopButton);
             }
         }
     };
+
+    /** Connect/Disconnect to Arduino on click */
+    public void onClickStart( View view ) {
+        HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
+        if (!usbDevices.isEmpty()) {
+            boolean keep = true;
+            for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
+                device = entry.getValue();
+                int deviceVID = device.getVendorId();
+                if ( deviceVID == ARDUINO_VID )
+                {
+                    PendingIntent pi = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+                    usbManager.requestPermission(device, pi);
+                    keep = false;
+                } else {
+                    connection = null;
+                    device = null;
+                }
+                if (!keep)
+                    break;
+            }
+        }
+    }
+    public void onClickStop( View view ) {
+        setUiEnabled(false);
+        serialPort.close();
+        tvAppend( statusText,"\nSerial Connection Closed! \n");
+    }
+    public void sendDoorSignal() {
+        if ( doInference() ) {
+            Toast.makeText( getBaseContext(), "Positive detection" , Toast.LENGTH_LONG ).show();
+            if ( serialPort != null && serialPort.isOpen() ) {
+                serialPort.write( new byte[]{YES_MASKED} );
+                Toast.makeText( getBaseContext(), "SENT MASK YAY UwU" , Toast.LENGTH_LONG ).show();
+            }
+        } else {
+            Toast.makeText( getBaseContext(), "Negative detection" , Toast.LENGTH_LONG ).show();
+            if ( serialPort != null && serialPort.isOpen() ) {
+                serialPort.write( new byte[]{NOT_MASKED} );
+                Toast.makeText( getBaseContext(), "SENT MASK NAY TwT" , Toast.LENGTH_LONG ).show();
+            }
+        }
+    }
+
+    // INFERENCE RELATED METHODS
+    /** Take photo and run inference on click */
+    public void onClickDetect( View view ) {
+        camera.startPreview();
+        camera.takePicture( null, null, jpegCallback );
+    }
 
     /** Uses the pre-prepared tflite model, bitmap and image processor to give output */
     private boolean doInference() {
@@ -324,5 +323,25 @@ public class MainActivity extends AppCompatActivity {
         matrix.postRotate(angle);
         return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
                 matrix, true);
+    }
+
+    // UI Utilities
+    /** enables and disables connect/disconnect button */
+    public void setUiEnabled(boolean bool) {
+        startButton.setEnabled(!bool);
+        stopButton.setEnabled(bool);
+        statusText.setEnabled(bool);
+    }
+    /** displays status in statusText */
+    private void tvAppend(TextView tv, CharSequence text) {
+        final TextView ftv = tv;
+        final CharSequence ftext = text;
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ftv.append(ftext);
+            }
+        });
     }
 }
